891268030a7ce87f07143cf2d8f1cd2d73c67d2d
Feng Liu <cdragon1612@yahoo.com>
2015-10-06 23:01:21 +0200

    update

--- js/index.js
+++ js/index.js
@@ -27,7 +27,10 @@ var app = {
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
-        document.getElementById('test').addEventListener('click', this.test, false);
+        document.getElementById('test_persistency').addEventListener('click', this.enablePersistence, false);
+        document.getElementById('disable_persistency').addEventListener('click', this.disablePersistence, false);
+        document.getElementById('test_read').addEventListener('click', this.readData, false);
+        document.getElementById('test_write').addEventListener('click', this.writeData, false);
     },
     // deviceready Event Handler
     //
@@ -48,22 +51,65 @@ var app = {
         console.log('Received Event: ' + id);
     },
 
-    test: function() {
+    enablePersistence: function() {
         var firebase = cordova.require("cordova/plugin/FireBase");
 
-        firebase.init('BenjaminDemo', function(success) {
-            alert("init success: " + success);
+        firebase.init('demobenjamin', function(success) {
 
             firebase.setPersistenceEnabled(true, function(success) {
                 alert("persistenceEnabled success");
+            }, function(fail) {
+                alert("persistenceEnabled failed: " + fail);
+            });
+          }, function(fail) {
+            alert("init failed: " + fail);
+          }
+        );
 
-                var testObj = { 'string': 'stringValue', 'int', 5, 'array': ['ele1', 'ele2'], 'object': {'key1': 'value1'} };
-                firebase.writeData(testObj, function(success) {
-                    alert("writeData success" + testObj.toString());
-                    firebase.readData(testObj, function(success) {
-                        console.log(success)
-                    });
-                });
+    },
+    
+    disablePersistence: function() {
+        var firebase = cordova.require("cordova/plugin/FireBase");
+        
+        firebase.init('demobenjamin', function(success) {
+                      
+            firebase.setPersistenceEnabled(false, function(success) {
+                alert("persistenceDisabled success");
+            }, function(fail) {
+                alert("persistenceDisabled failed: " + fail);
+            });
+        }, function(fail) {
+            alert("init failed: " + fail);
+        });
+        
+    },
+
+    readData: function() {
+        var firebase = cordova.require("cordova/plugin/FireBase");
+
+        firebase.init('demobenjamin', function(success) {
+            firebase.readData(function(success) {
+                console.log(success);
+                alert(success);
+            }, function(fail) {
+                alert("readData failed: " + fail);
+            });
+          }, function(fail) {
+            alert("init failed: " + fail);
+          }
+        );
+
+    },
+
+    writeData: function() {
+        var firebase = cordova.require("cordova/plugin/FireBase");
+
+        firebase.init('demobenjamin', function(success) {
+            var testObj = { 'string': 'stringValue', 'int': 5, 'array': ['ele1', 'ele2'], 'object': {'key1': 'value1'} };
+            firebase.writeData(testObj, function(success) {
+                alert("writeData success");
+            }, function(fail) {
+                alert("writeData failed: " + fail);
             });
           }, function(fail) {
             alert("init failed: " + fail);
